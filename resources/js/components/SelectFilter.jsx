7b870c782ccfd8b344dfef40a5d1d9f883045e19
Konstantin Kostadinov <kkostadinov@thinkweb.bg>
2023-02-05 18:23:48 +0200

    #6#9

---
+++ resources/js/components/SelectFilter.jsx
@@ -0,0 +1,72 @@
+import React, {useRef, useState} from "react";
+import "./SelectFilter.css";
+
+const findValue = (data, value) => {
+    const item = data.find((item) => item.value === value);
+    return item ? item.name : "";
+}
+
+const SelectFilter = ({data, onSelect, value}) => {
+    const [filter, setFilter] = useState(findValue(data, value));
+    const [filteredData, setFilteredData] = useState(data);
+    const [showOptions, setShowOptions] = useState(false);
+    const selectRef = useRef(null);
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+        setFilteredData(
+            data.filter((item) =>
+                item.name.toLowerCase().startsWith(event.target.name.toLowerCase())
+            )
+        );
+    };
+
+    const handleClickOutside = (event) => {
+        if(selectRef.current && !selectRef.current.contains(event.target)) {
+            setShowOptions(false);
+        }
+    };
+
+    const handleOptionClick = (item) => {
+        setFilter(item.name);
+        setShowOptions(false);
+        onSelect(item.value);
+    };
+
+    React.useEffect(() => {
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    });
+
+    return (
+        <div className="select-filter" ref={selectRef}>
+            <div className="select-filter-input">
+                <input
+                    type="text"
+                    className="form-control"
+                    id="selectFilter"
+                    value={filter}
+                    onChange={handleFilterChange}
+                    onFocus={() => setShowOptions(true)}
+                />
+            </div>
+            {showOptions && (
+                <ul className="select-filter-options">
+                    {filteredData.map((item) => (
+                        <li
+                            key={item.value}
+                            className="select-filter-option"
+                            onClick={() => handleOptionClick(item)}
+                        >
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default SelectFilter;
