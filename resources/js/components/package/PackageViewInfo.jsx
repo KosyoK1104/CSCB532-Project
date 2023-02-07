1b7023ff16080b3fe84967d217d38a90db3b827b
Nina Petrova <ninapetrova0202@gmail.com>
2023-02-07 23:05:16 +0200

    Packages

--- resources/js/components/package/PackageViewInfo.jsx
+++ resources/js/components/package/PackageViewInfo.jsx
@@ -1,83 +1,38 @@
-import {useParams} from "react-router-dom";
-import {useState} from "react";
-import Api from "../../services/Api";
 import InfoLine from "../../components/InfoLine";
-import LoaderProvider from "../LoaderProvider";
 
-const PackageView = () => {
-    const {id} = useParams();
-
-    let [status, setStatus] = useState('');
-    const onChangeStatus = (e) => {
-        setStatus(e.target.value);
-    }
-
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        let [clientPackage, setClientPackage] = useState({
-            id: id,
-            tracking_number: '',
-            office_id: '',
-            client_id: '',
-            delivery_type: '',
-            status: status,
-            price: '',
-            weight: '',
-            recipient_name: '',
-            recipient_phone_number: '',
-            recipient_address: '',
-        });
-
-        Api.post('/api/employees/packages', clientPackage)
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+const PackageView = ({clientPackage}) => {
 
     return (
-        <div className="container">
-            <div className="card">
-                <div className="card-header">
-                    <h3>Package {clientPackage.visual_id}</h3>
-                </div>
-                <LoaderProvider>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Tracking number" value={clientPackage.tracking_number}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Office# " value={clientPackage.office_id}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Client" value={clientPackage.client_id}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Delivery type" value={clientPackage.status}
-                                          onChange={onChangeStatus()}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Price" value={clientPackage.price}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Weight" value={clientPackage.weight}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Recipient name" value={clientPackage.recipient_name}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Recipient phone " value={clientPackage.recipient_phone_number}/>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <InfoLine label="Recipient address" value={clientPackage.recipient_address}/>
-                            </div>
-                        </div>
-                    </div>
-                </LoaderProvider>
+
+        <>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Tracking number" value={clientPackage.tracking_number}/>
             </div>
-        </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Office# " value={clientPackage.office_id}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Client" value={clientPackage.client_name}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Delivery type" value={clientPackage.status}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Price" value={clientPackage.price}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Weight" value={clientPackage.weight}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Recipient name" value={clientPackage.recipient_name}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Recipient phone " value={clientPackage.recipient_phone_number}/>
+            </div>
+            <div className="col-12 col-md-6">
+                <InfoLine label="Recipient address" value={clientPackage.recipient_address}/>
+            </div>
+        </>
     );
 }
 
--- resources/js/pages/employees/packages/PackageView.jsx
+++ resources/js/pages/employees/packages/PackageView.jsx
@@ -1,6 +1,8 @@
 import PackageViewInfo from "../../../components/package/PackageViewInfo";
 import {useParams} from "react-router-dom";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import Api from "../../../services/Api";
+import LoaderProvider from "../../../components/LoaderProvider";
 
 const EmployeePackageView = () => {
     let {id} = useParams();
@@ -8,7 +10,7 @@ const EmployeePackageView = () => {
         id: '',
         tracking_number: '',
         office: '',
-        client: '',
+        client_name: '',
         client_phone_number: '',
         delivery_type: '',
         status: '',
@@ -19,20 +21,28 @@ const EmployeePackageView = () => {
         recipient_address: '',
     });
 
+    useEffect(() => {
+        Api.get('/api/employees/packages/' + id)
+            .then(response => {
+                console.log(response.data.data);
+                setClientPackage(response.data.data);
+            })
+    }, []);
+
     return (
         <div className="container">
             <div className="card">
-                <div className="card-header">
-                    <h3>Package {clientPackage.tracking_number}</h3>
-                </div>
-                <div className="card-body">
-
-                    <PackageViewInfo></PackageViewInfo>
-                    <br></br>
-                    <div>
-                        <button className="btn btn-primary">Mark as delivered</button>
+                <LoaderProvider>
+                    <div className="card-header">
+                        <h3>Package {clientPackage.tracking_number}</h3>
+                        <div className="card-options">
+                            <button className="btn btn-primary">Mark as delivered</button>
+                        </div>
                     </div>
-                </div>
+                    <div className="card-body">
+                        <PackageViewInfo clientPackage={clientPackage}></PackageViewInfo>
+                    </div>
+                </LoaderProvider>
             </div>
         </div>
     );
